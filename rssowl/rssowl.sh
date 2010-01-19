c7d96be3fa2a0da0c738138895a172bef16239d4
Mikhail Kolesnik <mike@openbunker.org>
2010-01-19 20:00:46 +0200

    move rssowl quagga sancho php-fcgi-fpm to mike_attic

--- rssowl/rssowl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /usr/lib/rssowl &&
-#MOZILLA_FIVE_HOME=${MOZILLA_FIVE_HOME:=`cat /etc/ld.so.conf.d/xulrunner.conf`} exec ./RSSOwl
-exec ./RSSOwl
--- sancho/sancho.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cd /usr/lib/sancho &&
-exec ./sancho
